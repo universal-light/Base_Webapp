f0fc3b9628e0358a243aa04115ae97a10be3734e
universal-light <finn.business.pa@proton.me>
2026-06-26 15:28:38 +0200

    Update deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,1 +1,12 @@
 
+#!/usr/bin/env bash
+
+set -e
+
+kubectl apply -f namespace.yaml
+kubectl apply -f secret.yaml
+kubectl apply -f postgres.yaml
+kubectl apply -f fastapi.yaml
+
+kubectl get pods -n my-web
+kubectl get svc -n my-web
